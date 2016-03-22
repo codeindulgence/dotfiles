9e03f33f501f59449da44e232eb5355886f8da01
Nick Butler <nick@codeindulgence.com>
2016-03-22 21:54:51 +1100

    Fish: Just some comments

--- config/fish/functions/fish_right_prompt.fish
+++ config/fish/functions/fish_right_prompt.fish
@@ -1,18 +1,22 @@
 function fish_right_prompt
   printf '%s' (__fish_git_prompt)
 
+  # Ruby Version
   if set -q RUBY_VERSION
     echo_wrapped $RUBY_VERSION red
   end
 
+  # Command Duration
   if [ "$CMD_DURATION" -gt 0 ]
     set -l ms (numfmt --grouping $CMD_DURATION)
     echo_wrapped {$ms}ms magenta
   end
 
+  # Local .env file
   if set -q env_loaded
     echo_wrapped ENV red
   end
 
+  # PWD
   echo_wrapped (prompt_pwd) green
 end
