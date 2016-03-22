cf544bd710f7930d3c811d1f68f5d6ba99b60f9a
Nick Butler <nick@codeindulgence.com>
2016-03-22 23:34:38 +1100

    Fish: Pretty much do everythin for interactive only

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,13 +1,13 @@
-if not set -q TMUX
-  if not set -q SSH_CLIENT
-    tmux -f ~/.config/tmux/tmux.conf new -s Main
-  end
-end
-
 if status --is-interactive
+  if not set -q TMUX
+    if not set -q SSH_CLIENT
+      tmux -f ~/.config/tmux/tmux.conf new -s Main
+    end
+  end
+
   eval sh ~/.config/fish/base16-ocean.dark.sh
+  source ~/.config/fish/variables.fish
+  source /usr/local/share/chruby/chruby.fish
   loadenv
 end
 
-source ~/.config/fish/variables.fish
-source /usr/local/share/chruby/chruby.fish
