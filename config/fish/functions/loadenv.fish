66edbe818ae0a420079ce77914de443e818fc6b1
Nick Butler <nick@codeindulgence.com>
2016-03-25 22:45:20 +1000

    Fish: Super cool .env swapping

--- config/fish/functions/loadenv.fish
+++ config/fish/functions/loadenv.fish
@@ -1,8 +1,21 @@
 function loadenv
-  if [ -r .env ]
+  set -l env_name (basename $PWD)
+  if [ -r .env -a "$env_loaded" != $env_name ]
+    unloadenv
+    # Get the list of vars we're about to load
+    set -g env_vars (sed 's/^export \([A-z0-9_-]*\)=.*/\1/' .env)
+
+    # Save the old values if they exist
+    for var in $env_vars
+      set -g env_vars_old $env_vars_old "$$var"
+    end
+
+    # Now load .env
     echo Loaded Environment:
+    set_color green
     sed 's/^export/ /' .env
+    set_color normal
     source .env
-    and set -g env_loaded (basename $PWD)
+    and set -g env_loaded $env_name
   end
 end
