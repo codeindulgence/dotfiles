ef279273cc230fb7f5f0d203d7f078e2321157eb
Nick Butler <nick@codeindulgence.com>
2016-09-29 13:25:28 +1000

    Fish: Made f command case insensitive

--- config/fish/functions/f.fish
+++ config/fish/functions/f.fish
@@ -1,7 +1,7 @@
 function f
   if [ (count $argv) -gt 1 ]
-    find $argv[2] -name "*$argv[1]*"
+    find $argv[2] -iname "*$argv[1]*"
   else
-    find -name "*$argv[1]*"
+    find -iname "*$argv[1]*"
   end
 end
