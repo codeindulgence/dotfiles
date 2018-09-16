5d6cc813453c6449dbdba1febd2c7bf7df291ee6
Nick Butler <nick@codeindulgence.com>
2018-09-17 09:17:12 +1000

    Fish: Slightly different C-w binding

--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -2,4 +2,5 @@ function fish_user_key_bindings
   bind \ek history-token-search-backward
   bind \ej history-token-search-forward
   bind \cx\ce edit_command_buffer
+  bind \cw backward-kill-word
 end
