5a2753c4fd7bb9b8cb957cfebb86b8e05aec2a8e
Nick Butler <nick@codeindulgence.com>
2019-09-23 18:54:16 +1000

    Fish: Drop g prefix on sha1sum in loadenv

--- config/fish/functions/loadenv.fish
+++ config/fish/functions/loadenv.fish
@@ -1,6 +1,6 @@
 function loadenv
   if [ -r .env ]
-    if [ (gsha1sum .env) != "$env_hash" ]
+    if [ (sha1sum .env) != "$env_hash" ]
       unloadenv
       # Get the list of vars we're about to load
       set -g env_vars (sed -n 's/^export \([A-z0-9_-]*\)=.*/\1/p' .env)
@@ -18,7 +18,7 @@ function loadenv
         | sed 's/\(.*\(KEY\|PASS\|SECRET\).*\)=\(...\).*\(...\)/\1=\3********\4/I'
       set_color normal
       and set -g env_loaded (basename $PWD)
-      and set -g env_hash (gsha1sum .env)
+      and set -g env_hash (sha1sum .env)
     end
   end
 end
