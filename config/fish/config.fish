6ce45b74b3b51e3c43e6d50531491db5d6e5875d
Nick Butler <nick@codeindulgence.com>
2019-09-25 22:53:13 +1000

    Fish: Removed old stuff

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -10,18 +10,4 @@ if status --is-interactive
   source "$BASE16_SHELL/profile_helper.fish"
 
   source ~/.config/fish/variables.fish
-  set chruby /usr/local/share/chruby/chruby.fish
-  if [ -f $chruby ];
-    source $chruby
-  end
-  loadenv
-  chruby-auto
 end
-
-# tabtab source for sls package
-# uninstall by removing these lines or running `tabtab uninstall sls`
-[ -f /Users/nick/.config/yarn/global/node_modules/tabtab/.completions/sls.fish ]; and . /Users/nick/.config/yarn/global/node_modules/tabtab/.completions/sls.fish
-
-# tabtab source for serverless package
-# uninstall by removing these lines or running `tabtab uninstall serverless`
-[ -f /Users/nick/.config/yarn/global/node_modules/tabtab/.completions/serverless.fish ]; and . /Users/nick/.config/yarn/global/node_modules/tabtab/.completions/serverless.fish
