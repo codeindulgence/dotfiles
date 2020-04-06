7200474cd349b00f2f14ddcbfe04d774efe68436
Nick Butler <nick@codeindulgence.com>
2020-04-06 20:38:58 +1000

    Fish: l to use -A

--- config/fish/functions/l.fish
+++ config/fish/functions/l.fish
@@ -1,5 +1,4 @@
-if type -q lsd
-  function l
-    lsd -l --date relative --blocks permission,user,size,date,name --group-dirs first $argv
-  end
+# Defined in /var/folders/gg/7kd96zf934bbjgd1v7tq4hzc0000gn/T//fish.iiZ8Hm/l.fish @ line 2
+function l
+    lsd -lA --date relative --blocks permission,user,size,date,name --group-dirs first $argv
 end
