bded8603e5f5784071ed3963992d4623c784e8ee
Nick Butler <nick@codeindulgence.com>
2021-05-20 21:24:03 +1000

    Tmux: Cosmetic updates

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,7 +1,7 @@
 if status --is-interactive
   if not set -q TMUX
     if not set -q SSH_CLIENT
-      tmux -f ~/.config/tmux/tmux.conf new-session -A -s main
+      tmux -f ~/.config/tmux/tmux.conf new-session -A -s main -n Home
     end
   end
 
