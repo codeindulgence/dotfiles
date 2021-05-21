49f0a316c4a43d0d2cc804858f2ff5e7e20fea17
Nick Butler <nick@codeindulgence.com>
2021-05-21 10:09:41 +1000

    Fish: use z in command not found

--- config/fish/functions/__fish_command_not_found_handler.fish
+++ config/fish/functions/__fish_command_not_found_handler.fish
@@ -1,13 +1,5 @@
 function __fish_command_not_found_handler --on-event fish_command_not_found
-  # Attempt to run as a git command, capture output with errors
-  set -l git_attempt (git $argv 2>&1)
-
-  # If it's not a git command, use default error
-  echo $git_attempt | grep -q "not a git command"
-  and __fish_default_command_not_found_handler $argv[1]
-
-  # Otherwise show the git command output
-  or for line in $git_attempt;
-    echo $line
-  end
+  z $argv > /dev/null
+  and pwd
+  or __fish_default_command_not_found_handler $argv[1]
 end
