898645ee4dd017b3db2ba3a20b6735a80d2d56b8
Nick Butler <nick@codeindulgence.com>
2021-05-27 15:54:32 +1000

    Fish: Fix infinite nesting fish shells

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,28 +3,23 @@ if status --is-interactive
     if not set -q SSH_CLIENT
       tmux -f ~/.config/tmux/tmux.conf new-session -A -s main -n Home
     end
-  end
-
-  source ("/usr/local/bin/starship" init fish --print-full-init | psub)
-
-  set EDITOR nvim
-  set -x ANSIBLE_NOCOWS 1
-
-  if set -q TMUX
+  else
+    source ("/usr/local/bin/starship" init fish --print-full-init | psub)
     set -l window_name (tmux display -p '#{window_name}')
-    if [ "$window_name" != "Home" ]
+
+    # Bindings
+    set fish_key_bindings fish_default_key_bindings
+
+    # Biome
+    set -g _biome_mask_char ''
+
+    # Set tab width
+    tabs -2
+
+    if [ "$window_name" != "Home" -a "$window_name" != "fish" ]
       type -q $window_name
       and $window_name
       or z $window_name >/dev/null
     end
   end
-
-  # Bindings
-  set fish_key_bindings fish_default_key_bindings
-
-  # Biome
-  set -g _biome_mask_char ''
-
-  # Set tab width
-  tabs -2
 end
