5f98bf2389a6fa21f430736f58a1fc1545de5805
Nick Butler <nick@codeindulgence.com>
2021-05-28 11:55:47 +1000

    Fish: Bind backspace for fzf-recent-widget

--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -9,6 +9,22 @@ if type -q fzf_key_bindings
   fzf_key_bindings
 end
 
+function fzf-recent-widget
+  set -l command 'z -l | awk "{print \$2}" | grep -v "^$PWD\$"'
+  set -l hist_file ~/.local/share/fzf-history/recent
+  set -l fzf_cmd 'fzf --height 40% --reverse --history='$hist_file
+  eval "$command | $fzf_cmd | read -l result"
+
+  if [ -n "$result" ]
+    cd $result
+    commandline -f repaint
+  end
+end
+
+bind -k backspace \
+  'test (commandline) = ""; \
+    and tmuxpasskey do fzf-recent-widget; \
+    or commandline -f backward-delete-char'
 bind \ct 'tmuxpasskey do fzf-file-widget'
 bind \cr 'tmuxpasskey do fzf-history-widget'
 bind \ec 'tmuxpasskey do fzf-cd-widget'
