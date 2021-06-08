aaa47041c4fc294f8bca41dde515b90e640f6d3e
Nick Butler <nick@codeindulgence.com>
2021-06-08 14:28:42 +1000

    Set VGit highlights in vgit.lua

--- config/nvim/lua/plugins/vgit.lua
+++ config/nvim/lua/plugins/vgit.lua
@@ -1,6 +1,10 @@
 require('vgit').setup {
   blames_enabled = false,
-  hls = { VGitBlame = { fg = '#4c566a' } },
+  hls = {
+    VGitBlame =      { fg = '#4c566a' },
+    VGitSignAdd =    { fg = '#A3BE8C' },
+    VGitSignRemove = { fg = '#BF616A' }
+  },
   hunk_sign = { signs = {
     add    = { text = "┃" },
     remove = { text = "┃" },
