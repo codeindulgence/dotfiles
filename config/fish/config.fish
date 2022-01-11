8ef5434ad6cca80fb413fdb41735028e1e6299d2
Nick Butler <nick@codeindulgence.com>
2022-01-11 19:13:02 +1100

    Fish: Update path to starship

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,5 @@
 if status --is-interactive
-  source ("/usr/local/bin/starship" init fish --print-full-init | psub)
+  source ("/opt/homebrew/bin/starship" init fish --print-full-init | psub)
   if not set -q TMUX
     if not set -q SSH_CLIENT
       tmux -f ~/.config/tmux/tmux.conf new-session -A -s main -n Home
