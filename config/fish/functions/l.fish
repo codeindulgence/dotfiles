98d390b6cc13b246b259e927870f0ef6143df613
Nick Butler <nick@codeindulgence.com>
2022-01-11 19:13:25 +1100

    Fish: Remove -A (almost all) from l

--- config/fish/functions/l.fish
+++ config/fish/functions/l.fish
@@ -1,4 +1,3 @@
-# Defined in /var/folders/gg/7kd96zf934bbjgd1v7tq4hzc0000gn/T//fish.iiZ8Hm/l.fish @ line 2
 function l
-    lsd -lA --date relative --blocks permission,user,size,date,name --group-dirs first $argv
+    lsd -l --date relative --blocks permission,user,size,date,name --group-dirs first $argv
 end
