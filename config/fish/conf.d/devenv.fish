b2ce96a56d707adb36e0b09e9ac747442010245b
Nick Butler <nick@codeindulgence.com>
2023-09-15 13:08:05 +1000

    Fish: Conda integration

--- config/fish/conf.d/devenv.fish
+++ config/fish/conf.d/devenv.fish
@@ -31,4 +31,15 @@ function devenv --on-event fish_prompt
     end
   end
 
+  # Load Conda env
+  if type -q conda
+    if test -r .conda-env
+      if not set -q CONDA_DEFAULT_ENV
+        set -l conda_env (cat .conda-env)
+        eval conda "shell.fish" "hook" $argv | source
+        conda activate $conda_env
+      end
+    end
+  end
+
 end
---
+++ config/fish/functions/condon.fish
@@ -0,0 +1,3 @@
+function condon
+  eval /opt/homebrew/Caskroom/miniforge/base/bin/conda "shell.fish" "hook" $argv | source
+end
